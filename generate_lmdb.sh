a347890906d188114337f27b0604a73a90ae523b
Hzzone <Hzzone@outlook.com>
2017-09-07 22:12:57 +0800

    generate data

---
+++ generate_lmdb.sh
@@ -0,0 +1,2 @@
+/home/bw/code/caffe/build/tools/convert_imageset --resize_height=227 --resize_width=227 --shuffle /home/bw/loss-function-comparation/CASIA-WebFace train.txt train_227_lmdb
+/home/bw/code/caffe/build/tools/convert_imageset --resize_height=223 --resize_width=223 --shuffle /home/bw/loss-function-comparation/CASIA-WebFace train.txt train_223_lmdb
