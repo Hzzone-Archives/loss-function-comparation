d277d010980a4a2ee0ee5fc9a7eaebd7ef3fea8f
Hzzone <Hzzone@outlook.com>
2017-09-27 20:45:56 +0800

    refactor folder

---
+++ softmax_loss/train.sh
@@ -0,0 +1,8 @@
+cd /home/hzzone/loss-function-comparation/softmax_loss/AlexNet\ Fine-tuning &&
+sudo ~/caffe/build/tools/caffe train --solver solver.prototxt --weights ../../pretrained-models/bvlc_alexnet.caffemodel 2>&1 | tee train.log &&
+cd /home/hzzone/loss-function-comparation/softmax_loss/AlexNet\ Off-the-shelf &&
+sudo ~/caffe/build/tools/caffe train --solver solver.prototxt 2>&1 | tee train.log &&
+cd /home/hzzone/loss-function-comparation/softmax_loss/CaffeNet\ Fine-tuning &&
+sudo ~/caffe/build/tools/caffe train --solver solver.prototxt --weights ../../pretrained-models/bvlc_reference_caffenet.caffemodel 2>&1 | tee train.log &&
+cd /home/hzzone/loss-function-comparation/softmax_loss/CaffeNet\ Off-the-shelf &&
+sudo ~/caffe/build/tools/caffe train --solver solver.prototxt 2>&1 | tee train.log 
