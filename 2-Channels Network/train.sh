bf89f52acdc3730b52034037a3167f4f377a56b9
Hzzone <Hzzone@outlook.com>
2017-10-08 23:21:01 +0800

    fix 2-chanels train.sh bug

--- 2-Channels Network/train.sh
+++ 2-Channels Network/train.sh
@@ -1,4 +1,4 @@
 cd /home/hzzone/loss-function-comparation/2-Channels\ Network/AlexNet\ Off-the-shelf &&
-sudo ~/caffe/build/tools/caffe train --solver solver.prototxt --weights ../../pretrained-models/bvlc_alexnet.caffemodel 2>&1 | tee train.log && &&
+sudo ~/caffe/build/tools/caffe train --solver solver.prototxt 2>&1 | tee train.log &&
 cd /home/hzzone/loss-function-comparation/2-Channels\ Network/CaffeNet\ Off-the-shelf &&
 sudo ~/caffe/build/tools/caffe train --solver solver.prototxt 2>&1 | tee train.log 
